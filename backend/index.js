1375833efeda13a14f8d01c3fb86e3bef6db97af
Shivam-75 <shivampandey7518200305@gmail.com>
2025-09-02 15:45:07 +0530

    folder changing

--- backend/index.js
+++ backend/index.js
@@ -15,11 +15,19 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+// app.use(cors({
+//     origin: "https://pathology-project-git-main-ss-projects-38a3890a.vercel.app",
+//     methods: "GET, POST ,PUT ",
+//     credentials:true
+// }))
+
 app.use(cors({
     origin: "https://pathology-project-git-main-ss-projects-38a3890a.vercel.app",
-    methods: "GET, POST ,PUT ",
-    credentials:true
-}))
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true
+}));
+
 app.get("/", (req, res) => {
     res.send("welcome");
 })
