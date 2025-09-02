23204bb985c18a177e2e26693e108293d4ad0aa3
Shivam-75 <shivampandey7518200305@gmail.com>
2025-09-02 15:47:35 +0530

    folder changing

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 // }))
 
 app.use(cors({
-    origin: "https://pathology-project-git-main-ss-projects-38a3890a.vercel.app",
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true
