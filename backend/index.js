6ef5f9ece246e17ee9fc786afc2e5794f5ca3e7c
Shivam-75 <shivampandey7518200305@gmail.com>
2025-10-07 18:34:08 +0530

    backend change

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(cookieParser());
 
 const allowedOrigins = [
     "http://localhost:5173",
-    "https://pathology-project.vercel.app/"
+    "https://pathology-project.vercel.app"
 ];
 app.use(cors({
     origin: allowedOrigins,
